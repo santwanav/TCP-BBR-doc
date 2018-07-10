6f8c6e3145e85fd0db82431834b3be5664cc8789
santwana <vermasantwana@gmail.com>
2018-07-10 15:36:27 -0400

    Updated the script for other plots

--- r-scripts/stats.r
+++ r-scripts/stats.r
@@ -1,7 +1,9 @@
 library(ggplot2)
+library(grid)
+library(lattice)
 library(reshape)
 
-dir <- "~/Desktop/IITK/Summer\'18-NYO/Actual\ Work/TCP-BBR-project-documentation/results/variations/100/var1-30/"
+dir <- "~/Desktop/IITK/Summer\'18-NYU/Actual\ Work/TCP-BBR-project-documentation/results/variations/100/var10-2-3/"
 dat <- read.csv(paste(dir, "stats.csv", sep=""))
 #datm <- read.csv(paste(dir, "impairstats.csv", sep=""))
 
@@ -14,24 +16,53 @@ mdat <- melt(dat, id=c("ts", "phase"))
 #mdatm <- melt(datm, id=c("ts"))
 
 #mdatall <- rbind(mdat, mdatm)
-#mdatall$value <- as.numeric(as.character(mdatall$value))
+mdatall$value <- as.numeric(as.character(mdatall$value))
 mdat$value <- as.numeric(as.character(mdat$value))
 
-# The main variable is ts (i.e time). The values you want to plot for all the values of the time should be given in the 'fields' field.
+fields = c("bbr_bw", "bbr_min_rtt")
 
-fields = c("bbr_bw") #bbr_min_rtt", "bbr_cwnd_gain", "bbr_pacing_gain", "bbr_bw")
+# Full time
 q <- ggplot(mdat[mdat$variable %in% fields,]) + geom_line(aes(x=ts, y=value), size = 0.1)
 q <- q + geom_point(aes(x=ts, y=value, colour=phase), size=0.1)
 q <- q + scale_colour_brewer(palette="Set1") + facet_wrap(~ variable, ncol=1, scale="free_y")
-q <- q + scale_x_continuous("Time (s)") #limits=c( min(mdat$ts), min(mdat$ts)+30))
-q <- q + scale_y_continuous("Bandwidth (bps)", limits=c( 0, 2.5e+06))
+q <- q + scale_x_continuous("Time (s)")
 q <- q + theme(legend.position="bottom")
-png(paste(dir,"plot.png", sep=""))
+png(paste(dir,"bwrtt.png", sep=""))
 print(q)
 dev.off()
 
-r <- ggplot(dat) + geom_line(aes(x=(segs_out*s-bytes_acked), y=delivery_rate))
-png(paste(dir, "characteristic.png",sep=""))
+# First 60 secs
+r <- ggplot(mdat[mdat$variable %in% fields,]) + geom_line(aes(x=ts, y=value), size = 0.1)
+r <- r + geom_point(aes(x=ts, y=value, colour=phase), size=0.1)
+r <- r + scale_colour_brewer(palette="Set1") + facet_wrap(~ variable, ncol=1, scale="free_y")
+r <- r + scale_x_continuous("Time (s)", limits=c(min(mdat$ts), min(mdat$ts)+60))
+r <- r + theme(legend.position="bottom")
+png(paste(dir,"bwrtt-60secs.png", sep=""))
 print(r)
+dev.off()
+
+# First 30 secs
+s <- ggplot(mdat[mdat$variable %in% fields,]) + geom_line(aes(x=ts, y=value), size = 0.1)
+s <- s + geom_point(aes(x=ts, y=value, colour=phase), size=0.1)
+s <- s + scale_colour_brewer(palette="Set1") + facet_wrap(~ variable, ncol=1, scale="free_y")
+s <- s + scale_x_continuous("Time (s)", limits=c(min(mdat$ts), min(mdat$ts)+30))
+s <- s + theme(legend.position="bottom")
+png(paste(dir,"bwrtt-30secs.png", sep=""))
+print(s)
+dev.off()
+
+# First 15 secs
+t <- ggplot(mdat[mdat$variable %in% fields,]) + geom_line(aes(x=ts, y=value), size = 0.1)
+t <- t + geom_point(aes(x=ts, y=value, colour=phase), size=0.1)
+t <- t + scale_colour_brewer(palette="Set1") + facet_wrap(~ variable, ncol=1, scale="free_y")
+t <- t + scale_x_continuous("Time (s)", limits=c(min(mdat$ts), min(mdat$ts)+60))
+t <- t + theme(legend.position="bottom")
+png(paste(dir,"bwrtt-15secs.png", sep=""))
+print(t)
+dev.off()
+
+u <- ggplot(dat) + geom_line(aes(x=(segs_out*s-bytes_acked), y=delivery_rate))
+png(paste(dir, "characteristic.png",sep=""))
+print(u)
 dev.off()
 
